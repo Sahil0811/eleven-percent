b1e946f0d9e960cc7450960e98da4e88b69288c7
ramanveerji <ramandeepmatharu@gmail.com>
2025-01-15 03:35:45 +0530

    changed size again

--- eleven-percent/ElevenPercentApplicationContext.cs
+++ eleven-percent/ElevenPercentApplicationContext.cs
@@ -98,7 +98,7 @@ public class ElevenPercentApplicationContext : ApplicationContext
 
     private static Icon IconFromText(string str)
     {
-        Font fontToUse = new Font("Microsoft Sans Serif", 18, FontStyle.Regular, GraphicsUnit.Pixel);
+        Font fontToUse = new Font("Microsoft Sans Serif", 22, FontStyle.Regular, GraphicsUnit.Pixel);
         Brush brushToUse = new SolidBrush(Color.Black);
         Bitmap bitmapText = new Bitmap(24, 24);
         Graphics g = Graphics.FromImage(bitmapText);
